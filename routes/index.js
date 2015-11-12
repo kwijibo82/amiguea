45bcee59553af184b42ac71fef44ca7391e0123b
System Administrator <root@MacBook-Pro-de-Xavi.local>
2015-11-12 17:08:03 +0100

    2nd commit

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+﻿
+/*
+ * GET home page.
+ */
+
+exports.index = function (req, res) {
+    res.render('index', { title: 'Amiguea: ', year: new Date().getFullYear() });
+};
+
+exports.about = function (req, res) {
+    res.render('about', { title: 'Contacta con nosotros', year: new Date().getFullYear(), message: 'Contacta con nosotros en el siguiente formulario' });
+};
+
+exports.contact = function (req, res) {
+    res.render('contact',{ title: 'Iniciar sesión', year: new Date().getFullYear(), message: 'Inicia sesión en Amiguea:' });
+};
+
+

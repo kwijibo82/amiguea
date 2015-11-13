8f0949e9e9a66d82714695826c912f0136479c71
System Administrator <root@MacBook-Pro-de-Xavi.local>
2015-11-13 16:09:26 +0100

    OpenShift changes

--- app.js
+++ app.js
@@ -37,3 +37,11 @@ app.get('/contact', routes.contact);
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
 });
+
+
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
+ 
+server.listen(server_port, server_ip_address, function () {
+  console.log( "Listening on " + server_ip_address + ", server_port " + port )
+});
